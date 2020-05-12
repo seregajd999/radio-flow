688ea960c46493c74fbe7474c6a4369eac857ccc
seregajd999 <seregajd999@gmail.com>
2020-05-12 16:44:57 +0300

    2

--- src/panels/Main.js
+++ src/panels/Main.js
@@ -3,8 +3,7 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
 import OAudioPlayer from '@oovui/audio-player-react';
 
-import { Media, Player, controls } from 'react-media-player'
-const { PlayPause, MuteUnmute } = controls
+
 
 
 
@@ -20,21 +19,8 @@ export default class Main extends Component {
         url = 'http:\\air2.radiorecord.ru:9003\rr_320'
         title = "Record"
         author = "укеукек" 
-        
         />
 
-<Media>
-        <div className="media">
-          <div className="media-player">
-            <Player src="http:\\air2.radiorecord.ru:9003\rr_320" />
-          </div>
-          <div className="media-controls">
-            <PlayPause />
-            <MuteUnmute />
-          </div>
-        </div>
-      </Media>
-  
 
 
       </Container>
