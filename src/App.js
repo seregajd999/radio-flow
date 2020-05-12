486b35466af7b99482487ba4b8261c71d9dce3e8
seregajd999 <seregajd999@gmail.com>
2020-05-12 10:43:11 +0300

    Loading

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navabar from './panels/Navabar';
+
+class App extends React.Component {
+
+	render() {
+		return (
+			<>
+				<Navabar />
+			</>
+		);
+	}
+}
+
+export default App;
