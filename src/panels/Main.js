6d1fe8c11ec271ebfa718ae1042ec1ea50df9718
seregajd999 <seregajd999@gmail.com>
2020-05-12 20:22:09 +0300

    Update Main.js

--- src/panels/Main.js
+++ src/panels/Main.js
@@ -1,10 +1,36 @@
 import React, { Component } from 'react';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import OAudioPlayer from '@oovui/audio-player-react';
+
+import Reaudio from 'reaudio'
+import 'reaudio/build/index.css'
 
 
-
+const playlist = [
+  {
+      id: '1',
+      source: 'https://studio.bio/reaudio/iiwii.mp3',
+      trackName: 'IIWII',
+      trackArtist: 'Joshua Iz',
+      trackImage: 'https://studio.bio/reaudio/images/VIZLP1.jpg',
+      loop: true
+  },
+  {
+      id: '2',
+      source: [
+          'https://studio.bio/reaudio/Rafael.aif',
+          'https://studio.bio/reaudio/Rafael.mp3'
+      ],
+      trackName: 'Rafael',
+      trackArtist: 'Joshua Iz',
+  },
+  {
+      id: '3',
+      source: 'https://studio.bio/reaudio/Voyager_1.mp3',
+      trackName: 'Voyager 1',
+      trackArtist: 'Joshua Iz',
+  }
+]
 
 
 export default class Main extends Component {
@@ -15,12 +41,11 @@ export default class Main extends Component {
       <Container>
 
 
-        <OAudioPlayer 
-        url = 'http:\\air2.radiorecord.ru:9003\rr_320'
-        title = "Record"
-        author = "укеукек" 
-        />
-
+       
+      <div className="App">
+            <h1>Reaudio</h1>
+            <Reaudio playlist={playlist} />
+        </div>
 
 
       </Container>
