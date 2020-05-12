e4a2b08244ec2655b868719ba132a24209e1240a
seregajd999 <seregajd999@gmail.com>
2020-05-12 16:14:01 +0300

    Updates

---
+++ precache-manifest.471ae54cbaf959144e5c2152cee47e64.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b48ee230d6e423ec124d",
+    "url": "/ecomerce-app/static/css/main.5c7b7c0e.chunk.css"
+  },
+  {
+    "revision": "b48ee230d6e423ec124d",
+    "url": "/ecomerce-app/static/js/main.04970580.chunk.js"
+  },
+  {
+    "revision": "9d4188da6b21417c0d6b",
+    "url": "/ecomerce-app/static/js/runtime~main.b818b5c3.js"
+  },
+  {
+    "revision": "3aeb3057d621008ce3c5",
+    "url": "/ecomerce-app/static/css/2.8975c6cd.chunk.css"
+  },
+  {
+    "revision": "3aeb3057d621008ce3c5",
+    "url": "/ecomerce-app/static/js/2.98bf1d2a.chunk.js"
+  },
+  {
+    "revision": "ed559950faf2020f3594696139d22b4b",
+    "url": "/ecomerce-app/index.html"
+  }
+];
